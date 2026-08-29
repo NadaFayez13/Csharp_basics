d8950df643ec89e611661edc01ef09cfc64c820a
NadaFayez13 <naditafayez13@gmail.com>
2026-08-29 16:18:14 +0300

    Add project files.

---
+++ Assignment_one/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_one
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
