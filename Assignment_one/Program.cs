1a6f609df9d409683849164cc4e03b6273ee5846
NadaFayez13 <naditafayez13@gmail.com>
2026-08-29 16:49:00 +0300

    second answer

--- Assignment_one/Program.cs
+++ Assignment_one/Program.cs
@@ -11,6 +11,13 @@
             object obj = book;
 
             Console.WriteLine(obj);
+
+            // question two
+            Console.WriteLine(book.ToString());
+            Console.WriteLine(book.Equals(book));
+            Console.WriteLine(book.GetHashCode());
+            Console.WriteLine(book.GetType());
+
         }
     }
 }
